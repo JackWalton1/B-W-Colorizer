7f59f4dd4ef5cdb16cdf0b72362255b247cc7ba8
JackWalton1 <jwalto02@calpoly.edu>
2024-03-06 10:17:55 -0800

    colorizeVideo.py

--- colorizeVideo.py
+++ colorizeVideo.py
@@ -1,3 +1,5 @@
+"""The purpose of this file is to try to abstact away the file paths
+from colorizeVideo.py and turn a b&w video into color"""
 import argparse
 import matplotlib.pyplot as plt
 
@@ -6,7 +8,24 @@ from utils import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
+
+# Add path arguments
+parser.add_argument('-vn','--video_name', type=str, default='GrapesOfWrathTrailerClip',
+                    help='Name of the video clip')
+
 opt = parser.parse_args()
+
+input_frames_folder = "./frames/"+ opt.video_name+"/output/"
+eccv_frames_folder = "./frames/"+opt.video_name+"/eccv/"
+siggraph_frames_folder = "./frames/"+opt.video_name+"/siggraph/"
+eccv_output_video = "./videos_out/eccv/"+opt.video_name+".mp4"
+siggraph_output_video = "./videos_out/siggraph/"+opt.video_name+".mp4"
+
+video_path = "./videos/" +opt.video_name+".mp4"
+output_folder = "./frames/" +opt.video_name + "/output"
+
+#b&w video -> b&w frames
+video_to_frames(video_path, output_folder)
 
 # load colorizers
 colorizer_eccv16 = eccv16(pretrained=True).eval()
@@ -17,10 +36,11 @@ if(opt.use_gpu):
 
 # Example usage
 i = 0
-for i in range(countFrames('./frames/GrapesOfWrathTrailerClip/output/')):
-# for i in range(10):
+# b&w frames to colorized frames
+for i in range(countFrames(input_frames_folder)):
     code = int_to_str_with_leading_zeros(i)
-    defaultPath = './frames/GrapesOfWrathTrailerClip/output/frame_' + str(code) + '.png'
+    defaultPath = input_frames_folder+ 'frame_' + str(code) + '.png'
+
     # default size to process images is 256x256
     # grab L channel in both original ("orig") and resized ("rs") resolutions
     img = load_img(defaultPath)
@@ -34,18 +54,17 @@ for i in range(countFrames('./frames/GrapesOfWrathTrailerClip/output/')):
     out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
     out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
-    #save the eccv version
-    plt.imsave('./frames/GrapesOfWrathTrailerClip/eccv/colorFrame_%s.png'%code, out_img_eccv16)
-    plt.imsave('./frames/GrapesOfWrathTrailerClip/siggraph/colorFrame_%s.png'%code, out_img_siggraph17)
+    # Save colorized frames using eccv_output_folder and siggraph_output_folder
+    if not os.path.exists(eccv_frames_folder):
+        print("No folder named: ", eccv_frames_folder,"\nMaking the folder.")
+        os.makedirs(eccv_frames_folder)
+    if not os.path.exists(siggraph_frames_folder):
+        print("No folder named: ", siggraph_frames_folder,"\nMaking the folder.")
+        os.makedirs(siggraph_frames_folder)
+    plt.imsave(os.path.join(eccv_frames_folder, 'colorFrame_%s.png' % code), out_img_eccv16)
+    plt.imsave(os.path.join(siggraph_frames_folder, 'colorFrame_%s.png' % code), out_img_siggraph17)
 
 
-eccv_input_folder = "./frames/GrapesOfWrathTrailerClip/eccv"
-siggraph_input_folder = "./frames/GrapesOfWrathTrailerClip/siggraph"
-eccv_output_video_path = "./videos_out/eccv/GrapesOfWrathTrailerClipColor.mp4"
-siggraph_output_video_path = "./videos_out/siggraph/GrapesOfWrathTrailerClipColor.mp4"
-
-# print(input_folder)
-# print(output_video_path)
-
-frames_to_video(eccv_input_folder, eccv_output_video_path)
-frames_to_video(siggraph_input_folder, siggraph_output_video_path)
+#colorized franes to colorized video
+frames_to_video(eccv_frames_folder, eccv_output_video)
+frames_to_video(siggraph_frames_folder, siggraph_output_video)
