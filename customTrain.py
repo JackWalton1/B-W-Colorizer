091401968523094d5c4adff7d1b3392575119af9
JackWalton1 <jwalto02@calpoly.edu>
2024-03-12 15:39:53 -0700

    train whole data set for 1 epoch

--- customTrain.py
+++ customTrain.py
@@ -18,20 +18,39 @@ class ColorizationDataset(Dataset):
     def __init__(self, root_folder, transform=None):
         self.root_folder = root_folder
         self.transform = transform
-        self.gray_folder = os.path.join(root_folder, 'Gray/Apple')
-        self.colorful_folder = os.path.join(root_folder, 'ColorfulOriginal/Apple')
-        self.image_list = os.listdir(self.gray_folder)
+        self.image_list = []
+
+        # Traverse through each subfolder in the 'Gray' directory
+        for folder_name in os.listdir(os.path.join(root_folder, 'Gray')):
+            gray_folder = os.path.join(root_folder, 'Gray', folder_name)
+            colorful_folder = os.path.join(root_folder, 'ColorfulOriginal', folder_name)
+
+            # Check if the subfolder contains 'Gray' and 'ColorfulOriginal' folders
+            if os.path.exists(gray_folder) and os.path.exists(colorful_folder):
+                # Get list of images in the 'Gray' folder
+                gray_images = os.listdir(gray_folder)
+
+                # Check if the same number of images exist in 'ColorfulOriginal' folder
+                if len(gray_images) == len(os.listdir(colorful_folder)):
+                    # Add each image pair to the image list
+                    for image_name in gray_images:
+                        gray_image_path = os.path.join(gray_folder, image_name)
+                        colorful_image_path = os.path.join(colorful_folder, image_name)
+                        self.image_list.append((gray_image_path, colorful_image_path))
+
+        print(f"Total images found: {len(self.image_list)}")
+
 
     def __len__(self):
         return len(self.image_list)
 
     def __getitem__(self, idx):
         # Load grayscale image
-        gray_image_path = os.path.join(self.gray_folder, self.image_list[idx])
+        gray_image_path = self.image_list[idx][0]
         gray_image = Image.open(gray_image_path).convert("L")
 
         # Load corresponding colorized image
-        colorful_image_path = os.path.join(self.colorful_folder, self.image_list[idx])
+        colorful_image_path = self.image_list[idx][1]
         colorful_image = Image.open(colorful_image_path).convert("RGB")
 
         if self.transform:
@@ -78,7 +97,7 @@ train_loader = DataLoader(train_dataset, batch_size=30, shuffle=True, collate_fn
 # val_dataset = ColorizationDataset(root_folder=val_folder)
 # val_loader = DataLoader(val_dataset, batch_size=30, shuffle=False)
 
-num_epochs = 10
+num_epochs = 1
 log_interval = 10  # Print the training loss every 10 batches
 optimizer = optim.Adam(model.parameters()) #, lr=0.001)
 
@@ -102,8 +121,8 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if batch_idx % log_interval == 0:
-            print(f"Epoch {epoch}, Batch {batch_idx}, Loss: {loss.item()}")
+        # if batch_idx % log_interval == 0:
+        print(f"Epoch {epoch}, Batch {batch_idx}, Loss: {loss.item()}")
             
     # Optionally, evaluate the model on the validation set after each epoch
 torch.save(model.state_dict(), 'custom_trained_model.pth')
